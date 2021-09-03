a1830ac06c7df053efdb2bce9c0163b30f918069
aggithubuser <bodijke@gmail.com>
2021-09-03 16:55:21 +0300

    process.env.NEXT_PUBLIC_DOMAIN_URL

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -24,14 +24,14 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 };
 
 async function publishPost(id: number): Promise<void> {
-  await fetch(`${process.env.DOMAIN_URL}/api/publish/${id}`, {
+  await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/publish/${id}`, {
     method: "PUT",
   });
   await Router.push("/");
 }
 
 async function deletePost(id: number): Promise<void> {
-  await fetch(`${process.env.DOMAIN_URL}/api/post/${id}`, {
+  await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/post/${id}`, {
     method: "DELETE",
   });
   Router.push("/");
