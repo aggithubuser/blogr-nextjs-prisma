1ffec01d839f538274491a35f593d5ca4c12a39a
aggithubuser <bodijke@gmail.com>
2021-09-03 17:04:58 +0300

    upd

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -24,14 +24,14 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 };
 
 async function publishPost(id: number): Promise<void> {
-  await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/publish/${id}`, {
+  await fetch(`/api/publish/${id}`, {
     method: "PUT",
   });
   await Router.push("/");
 }
 
 async function deletePost(id: number): Promise<void> {
-  await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_URL}/api/post/${id}`, {
+  await fetch(`/api/post/${id}`, {
     method: "DELETE",
   });
   Router.push("/");
